db63cc88dc437adfb5e7e3f2946a9094af29d96c
timofeevasp <Svetaptimofeeva@yandex.ru>
2026-09-24 12:32:34 +0300

    исправила

--- get-led/bin-counter.py
+++ get-led/bin-counter.py
@@ -6,6 +6,8 @@ GPIO.setup(leds, GPIO.OUT)
 GPIO.output(leds,0)
 up=9
 down=10
+GPIO.setup(up, GPIO.IN)
+GPIO.setup(down, GPIO.IN)
 num=0
 def dec2bin(value):
     return [int(element) for element in bin(value)[2:].zfill(8)]
